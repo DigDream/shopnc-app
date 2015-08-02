9475267b32416baebb027a2228464cfba47aa1e5
dubuqingfeng <1135326346@qq.com>
2015-08-02 10:58:43 +0800

    增加渐变跳转Activity
    Signed-off-by: dubuqingfeng <1135326346@qq.com>

--- Src/app/src/main/java/com/daxueoo/shopnc/ui/activity/SplashActivity.java
+++ Src/app/src/main/java/com/daxueoo/shopnc/ui/activity/SplashActivity.java
@@ -1,7 +1,52 @@
 package com.daxueoo.shopnc.ui.activity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+
+import com.daxueoo.shopnc.MainActivity;
+import com.daxueoo.shopnc.R;
+
 /**
  * Created by user on 15-8-2.
  */
 public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        final View view = View.inflate(this, R.layout.activity_splash, null);
+        setContentView(view);
+
+        AlphaAnimation animation = new AlphaAnimation(0.0f, 1.0f);
+        animation.setDuration(2000);
+        view.startAnimation(animation);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+
+            @Override
+            public void onAnimationEnd(Animation arg0) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation arg0) {
+            }
+
+            @Override
+            public void onAnimationStart(Animation arg0) {
+            }
+
+        });
+    }
 }
