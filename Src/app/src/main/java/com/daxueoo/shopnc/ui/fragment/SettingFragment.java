293002cac88acc03809028620b9d6e695b399144
geekmc <1409052439@qq.com>
2015-08-05 16:01:24 +0800

    新增退出登录

--- Src/app/src/main/java/com/daxueoo/shopnc/ui/fragment/SettingFragment.java
+++ Src/app/src/main/java/com/daxueoo/shopnc/ui/fragment/SettingFragment.java
@@ -1,9 +1,14 @@
 package com.daxueoo.shopnc.ui.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
 
 import com.daxueoo.shopnc.R;
+import com.daxueoo.shopnc.ui.activity.LoginActivity;
+import com.daxueoo.shopnc.ui.activity.SettingActivity;
 
 /**
  * Created by user on 15-8-2.
@@ -13,5 +18,23 @@ public class SettingFragment extends PreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
+    }
+    //退出登录的design
+    public  boolean  onPreferenceTreeClick (PreferenceScreen preferenceScreen,Preference preference){
+        // 对控件进行操作
+     if(preference.getKey().equals("tuichu")){
+
+         // 创建一个新的Intent，
+         // 函数如果返回true， 则跳转至该自定义的新的Intent ;
+         // 函数如果返回false，则跳转至xml文件中配置的Intent ;
+         Intent intent= new Intent();
+         intent.setClass(getActivity(),LoginActivity.class);
+    ;
+         startActivity(intent);
+
+         return true;
+     }
+        return  false;
     }
 }
